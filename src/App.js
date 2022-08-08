7b68749d6c6abc310e79322c87d8daaeb6dd5ecc
Shashank-Jha <53325972+Shashank-Jha@users.noreply.github.com>
2022-08-08 10:11:36 +0530

    FEAT : basic setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+import { Routes } from './components/Routes'
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+
+const App = () => {
+    const [darkTheme, setdarkTheme] = useState(false);
+
+    return (
+        <div className = {darkTheme ? 'dark':''}>
+            <div className = "bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+                <Navbar darkTheme={darkTheme} setdarkTheme={setdarkTheme}/>
+                <Routes/>
+                <Footer/>
+            </div>
+        </div>
+    )
+}
+
+export default App
